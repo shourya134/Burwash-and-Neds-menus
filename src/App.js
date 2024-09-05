72875de30ba196903d97ef43e278dedd96f8601f
Shourya Sharma <50227800+shourya134@users.noreply.github.com>
2024-09-05 10:46:07 -0400

    router dom added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import './App.css';
 import React from 'react';
 import {BDH} from './BDH'
 import {NC} from './NC'
-
+import { Routes, Route } from 'react-router-dom';
+import { DiVim } from 'react-icons/di';
 
 function App() {
   return (
-    
     <div className="App-header">
-      <BDH />
+      <Routes> {/* The Switch decides which component to show based on the current URL.*/}
+        <Route path='/bdh' element={<BDH/>}/>
+        <Route path='/nc' element={<NC/>}/>
+    </Routes>
     </div>
+    
+
   );
 }
 
